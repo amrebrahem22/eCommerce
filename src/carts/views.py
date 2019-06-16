a9ccf4492e4186eaaad312e2fb2534f22a4f200e
amrebrahem226 <amrebrahem226@gmail.com>
2019-06-16 15:12:06 +0200

    modifing the cart view

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
 
+from .models import Cart
+
+
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    print('New Cart created')
+    return cart_obj
+
 def cart_home(request):
-    return render(request, 'carts/home.html', {})
+    # request.session['cart_id']  = "12"
+    cart_id = request.session.get("cart_id", None)
+    print(cart_id)
+    if cart_id is None:
+        cart_obj = cart_create()
+        request.session['cart_id'] = cart_obj.id
+    else:
+        qs = Cart.objects.filter(id=cart_id)
+        if qs.count() == 1:
+            print('Cart ID exists')
+            cart_obj = qs.first()
+        else:
+            cart_obj = cart_create()
+            request.session['cart_id'] = cart_obj.id
+    print(cart_id)
+    return render(request, "carts/home.html", {})
